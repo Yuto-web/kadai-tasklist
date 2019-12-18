92699b6b6ba7f49b39a4fc183a1903fdec6fd44b
yuto.ishikawa <yishikawa246@gmail.com>
2019-12-18 22:02:30 +0000

    add correct_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.order(id: :desc).page(params[:id]).per(25)
-  end
-
-  def show
-  end
 
   def new
     @user = User.new
